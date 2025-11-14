d07fc6a5603d65c7f71f19c533a221e9610264a8
Ajitha <ajitha.wipro@gmail.com>
2025-11-14 14:11:11 +0530

    added authentication

--- Q12/Q12_Signup_Assignment.js
+++ Q12/Q12_Signup_Assignment.js
@@ -1,7 +1,7 @@
 function signUp(UserName)
 {
     let arr=["Alice","Bob","Dave"];
-    for(i=0;i<arr.length;i++)
+    for(let i=0;i<arr.length;i++)
     {
         if(arr[i]===UserName)
         {
@@ -14,5 +14,34 @@ function signUp(UserName)
     return "SignUp Successful, Please Login!"
 }
 
-let res=signUp("Alice");
+let res=signUp("Kim");
 console.log(res);
+
+function loginPage(Username, Password)
+ {
+    let arr = ["Alice", "David", "Bob"];
+    let userFound = false; 
+    for (let i = 0; i < arr.length; i++)
+         {
+        if (arr[i] === Username)
+         {
+            userFound = true; 
+            if (Password === "Emp@123")
+           {
+                return "Login Successful!";
+            } else 
+            {
+                return "Wrong Password";
+            }
+        }
+    }
+    if (!userFound) 
+    {
+        return "User Not Found, Please SignUp!";
+    }
+}
+
+let res1 = loginPage("Alice", "Emp@123");
+console.log(res1); 
+console.log(loginPage("Alice", "abc"));
+console.log(loginPage("Charlie", "Emp@123"));
